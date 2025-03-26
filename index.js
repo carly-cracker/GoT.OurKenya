e012ea14f3c2b0ea731f8d5d2a59a8cea57be583
Korir Carlos <carlkorir226@gmail.com>
2025-03-26 23:55:00 +0300

    adds html file css dbjson and some event listener

---
+++ index.js
@@ -0,0 +1,98 @@
+document.addEventListener('DOMContentLoaded',main)
+
+const topBar = document.getElementById("safari-packages")
+function fetchImages (){
+    return fetch('http://localhost:3000/safaris')
+    .then ((res) => res.json())
+    .then((safaris) => {
+        topBar.classList.add('image-gallery');
+            
+            safaris.forEach(safari => {
+                const imgSafari = document.createElement("img");
+                imgSafari.src = safari.image;
+                imgSafari .alt = safari.name;
+
+                const imageContainer = document.createElement("div");
+                imageContainer.classList.add('gallery-item');
+                
+                imageContainer.appendChild(imgSafari);
+
+                const nameSafari = document.createElement("p");
+                nameSafari.textContent = safari.name;
+                nameSafari.classList.add('image-caption');
+                imageContainer.appendChild(nameSafari);
+
+                const safariPrice = document.createElement("p");
+                safariPrice.textContent = safari.pricing;
+                safariPrice.classList.add('image-price');
+                imageContainer.appendChild(safariPrice);
+
+                const safariSite =document.createElement("h4")
+                safariSite.textContent = safari.location
+                safariSite.classList.add('image-caption')
+                imageContainer.appendChild(safariSite)
+
+                const exploreButton = document.createElement("button");
+                exploreButton.textContent = "Explore";
+                exploreButton.classList.add("explore-btn")
+                imageContainer.appendChild(exploreButton)
+
+                imageContainer.addEventListener("click", ()=>handleSafariClick(safari))
+
+                topBar.appendChild(imageContainer);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching images:", error)
+        });
+}
+
+const safariInfo = document.getElementById("safari-detail")
+function fetchDetails(){
+ return fetch(`http://localhost:3000/safaris/${safariId}`)
+ .then((res)=>res.json())
+ .then(handleSafariClick)
+ .catch(error => console.error("Error fetching safari details:", error))
+}
+
+function handleSafariClick(safari){
+    safariInfo.innerHTML=""
+    const safariName = document.createElement("h3")
+    safariName.innerHTML =safari.name
+    safariName.classList.add('image-caption')
+    safariName.style.fontSize = "25px"
+    safariName.style.textAlign = "center"
+    
+
+    const imgSafari = document.createElement("img")
+    imgSafari.src = safari.image
+    imgSafari.alt = safari.name
+    imgSafari.style.width = "700px"; 
+    imgSafari.style.height = "500px"; 
+    imgSafari.style.objectFit = "cover"; 
+    imgSafari.style.borderRadius = "10px"; 
+    imgSafari.style.boxShadow = "0px 4px 10px rgba(0, 0, 0, 0.5)";
+    imgSafari.style.display = "block"
+    imgSafari.style.margin = "20px auto"
+
+    const safariDetails = document.createElement("h6")
+    safariDetails.style.color= '#fff'
+    safariDetails.style.fontSize = "16px"
+    safariDetails.style.lineHeight = "1.6";  
+    safariDetails.style.padding = "10px";  
+    safariDetails.style.background = "rgba(0, 0, 0, 0.6)"; 
+    safariDetails.style.borderRadius = "10px"; 
+    safariDetails.style.textAlign = "center"; 
+    safariDetails.style.marginTop = "10px"
+
+    safariDetails.innerHTML = safari.details
+    safariInfo.appendChild(safariName)
+    safariInfo.appendChild(imgSafari)
+    safariInfo.appendChild(safariDetails)
+    console.log("happy")
+}
+
+function main(){
+    fetchImages()
+    
+}
